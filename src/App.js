4c35cfd1947745080aa73d573aef7475a9f57bef
joanita-51 <nakityojoanita51@gmail.com>
2023-07-20 10:04:08 +0300

    updating the boiler plate App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is a beautiful tecnology to learn
         </a>
       </header>
     </div>
