bcae575e57658818754f7439c8f7fba7123b7baa
joanita-51 <nakityojoanita51@gmail.com>
2023-07-25 11:05:00 +0300

    Converting components to use dynamic data

--- src/components/HotelInfo.js
+++ src/components/HotelInfo.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import arrivals from '../components/data/arrival_info.json'
+import services from "../components/data/services.json"
+import accessibility from "../components/data/accessibility.json"
 
 function HotelInfo() {
   return (
@@ -10,52 +13,35 @@ function HotelInfo() {
             <section id="arrivalinfo">
                 <h2>Arrival Information</h2>
                 <ul>
-                <li><strong>Check-in:</strong> 3:00 PM</li>
-                <li><strong>Check-out:</strong> 11:00 AM</li>
-                <li><strong>Parking:</strong> Self-parking in the underground garage is ￡15 per day and valet-parking is ￡50 per day.</li>
-                <li><strong>Airport Shuttle:</strong> Our complimentary airport shuttles leave every hour on the hour, and make trips to Heathrow and Gatwick airports.</li>
-                <li><strong>Trains:</strong> The nearest Underground station is at Leicester Square.</li>
-                <li><strong>Pet Policy:</strong> Pets of all sizes and types are allowed in designated pet rooms, and the specified common areas. Service animals are allowed everywhere.</li>
+                    {
+                        arrivals.map((arrival)=>
+                            <li><strong>{arrival.name}</strong>{arrival.info}</li>                        
+                        )
+                    }
+                
                 </ul>
             </section>
             <section className="checklist" id="services">
                 <h2>Services and Amenities</h2>
                 <p>Our services and amenities are designed to make your travel easy, your stay comfortable, and your experience one-of-a-kind.</p>
                 <ul>
-                <li>Indoor pool</li>
-                <li>24-hour fitness center</li>
-                <li>Massage therapy</li>
-                <li>Full service spa</li>
-                <li>In-room jacuzzi tubs</li>
-                <li>Rooftop café  &amp; smoothie bar</li>
-                <li>Coffee bar  &amp; pastry shop</li>
-                <li>Traditional continental breakfast</li>
-                <li>24-hour concierge service</li>
-                <li>Business center</li>
-                <li>Complimentary wireless service</li>
-                <li>Laundry &amp; dry cleaning service</li>
-                <li>Daily paper</li>
-                <li>Certified "green" hotel</li>
-                <li>Pet-friendly rooms  &amp; common areas</li>
+                    {
+                        services.map((service)=>
+                            <li>{service.name}</li>                        
+                        )
+                    }
                 </ul>
             </section>
             <section className="checklist" id="accessibility">
                 <h2>Accessibility</h2>
                 <p>We're committed to maintaining the same quality of service for every individual. We offer the following facilities for those with special needs:</p>
                 <ul>
-                <li>Grab bars on tub walls</li>
-                <li>Shower chairs</li>
-                <li>Hand held shower sprayers</li>
-                <li>Higher toilets &amp; toilet modifiers</li>
-                <li>Lower sink faucet handles</li>
-                <li>Wheelchair clearance under sinks &amp; vanity</li>
-                <li>Lower racks in closet</li>
-                <li>TDD machines</li>
-                <li>Telephone light signalers  &amp; smoke alarms</li>
-                <li>Telephone amplification handsets</li>
-                <li>Closed captioned television converters</li>
-                <li>Vibrating alarm clocks</li>
-                <li>Telephones with volume control</li>
+                    {
+                        accessibility.map((accessibility)=>
+                            <li>{accessibility.name}</li>                        
+                        )
+                    }
+
                 </ul>
             </section>
         </article>
